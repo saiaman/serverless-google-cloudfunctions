ec66e9971fb84827cc0166dd54e286c4788c7e90
Rui Albuquerque <rui.albuquerque@morning.com>
2019-08-23 17:02:35 +0200

    clean consoles.log

--- shared/setDeploymentBucketName.js
+++ shared/setDeploymentBucketName.js
@@ -5,7 +5,6 @@ const _ = require('lodash');
 
 module.exports = {
   setDeploymentBucketName() {
-    console.log("setDeploymentBucketName")
     // set a default name for the deployment bucket
     const service = this.serverless.service.service;
     const stage = this.options.stage;
@@ -14,8 +13,6 @@ module.exports = {
 
     this.serverless.service.provider.deploymentBucketName = name;
 
-    // console.log(this.serverless.service.provider.region)
-    // process.exit()
     // check if there's already a deployment and update if available
     const params = {
       project: this.serverless.service.provider.project,
@@ -26,7 +23,6 @@ module.exports = {
       .then((response) => {
         if (!_.isEmpty(response) && response.resources) {
           const regex = new RegExp(`sls-${service}-${stage}-.+`);
-          console.log("sls-${service}-${stage}-.+")
           let deploymentBucket = response.resources
             .find(resource => (resource.type === 'storage.v1.bucket'
               && resource.name.match(regex) && resource.finalProperties.indexOf(this.serverless.service.provider.region) > -1));
